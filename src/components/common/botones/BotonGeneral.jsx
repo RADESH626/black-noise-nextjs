2655396a48b8520e26f3e88318db5023002fbf54
cristian342 <cridtianito@gmail.com>
2025-06-21 00:14:30 -0500

    fix: Resolved merge conflicts in BotonGeneral.jsx

--- src/components/common/botones/BotonGeneral.jsx
+++ src/components/common/botones/BotonGeneral.jsx
@@ -9,13 +9,12 @@ function BotonGeneral({ children, onClick, type = 'button', disabled = false, cl
 
     switch (variant) {
         case 'primary':
-<<<<<<< HEAD
             variantStyles = 'btn-gradient-primary';
-            textStyles = 'text-white'; // Texto blanco para el botón primario
+            textStyles = 'text-white';
             break;
         case 'secondary':
-            variantStyles = 'btn-gradient-gray-to-white'; // Clase CSS personalizada para el gradiente
-            textStyles = 'text-white';
+            variantStyles = 'btn-gradient-gray-to-white';
+            textStyles = 'text-black';
             break;
         case 'danger':
             variantStyles = 'btn-gradient-danger';
@@ -27,29 +26,10 @@ function BotonGeneral({ children, onClick, type = 'button', disabled = false, cl
             break;
         case 'info':
             variantStyles = 'btn-gradient-info';
-=======
-            variantStyles = 'bg-gradient-primary-button';
-            textStyles = 'text-white'; // Texto blanco para el botón primario
-            break;
-        case 'secondary':
-            variantStyles = 'bg-gradient-secondary-button';
-            textStyles = 'text-black';
-            break;
-        case 'danger':
-            variantStyles = 'bg-gradient-danger-button';
-            textStyles = 'text-white';
-            break;
-        case 'success':
-            variantStyles = 'bg-gradient-success-button';
-            textStyles = 'text-white';
-            break;
-        case 'info':
-            variantStyles = 'bg-gradient-info-button';
->>>>>>> c32cb53 (primer commit)
             textStyles = 'text-white';
             break;
         default:
-            variantStyles = 'bg-gradient-primary-button';
+            variantStyles = 'btn-gradient-primary';
             textStyles = 'text-white';
             break;
     }
