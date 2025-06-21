30f3e05249d8b420c1d304546ef9209dfa602f7d
cristian342 <cridtianito@gmail.com>
2025-06-21 00:11:28 -0500

    fix: Resolved merge conflicts in BotonDescargar.jsx

--- src/components/common/botones/BotonDescargar.jsx
+++ src/components/common/botones/BotonDescargar.jsx
@@ -1,16 +1,10 @@
 "use client";
 
-
 import React from 'react';
-<<<<<<< HEAD
 import BotonGeneral from './BotonGeneral';
-=======
->>>>>>> c32cb53 (primer commit)
-
 
 export function BotonDescargar({ children, onClick, type = 'button', disabled = false, className = '' }) {
     return (
-<<<<<<< HEAD
         <BotonGeneral
             type={type}
             onClick={onClick}
@@ -22,23 +16,7 @@ export function BotonDescargar({ children, onClick, type = 'button', disabled = 
         >
             {children}
         </BotonGeneral>
-=======
-        <button
-            type={type}
-            onClick={onClick}
-            disabled={disabled}
-            className={`px-8 py-2 bg-gradient-to-r from-[#FFFFFF] to-[#3656e2] text-black font-semibold 
-            ${!disabled ? 'hover:from-[#4866ec] hover:to-[#FFFFFF]' : 'opacity-50 cursor-not-allowed'} 
-            rounded-[20px] transition duration-300 ${className}`}
-        >
-            {children}
-        </button>
->>>>>>> c32cb53 (primer commit)
     );
 }
 
 export default BotonDescargar;
-<<<<<<< HEAD
-=======
-
->>>>>>> c32cb53 (primer commit)
