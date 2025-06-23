40dad29cf0eb2931a00f05f6dbc4623836fac021
cristian342 <cridtianito@gmail.com>
2025-06-23 12:45:22 -0500

    feat: Create supplier payments view

---
+++ src/components/layout/proveedor/dashboards/PagosDashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PagosDashboard = () => {
+  return (
+    <div>
+      <h1>Panel de Pagos del Proveedor</h1>
+      <p>Aquí se mostrará la información de pagos para el proveedor.</p>
+    </div>
+  );
+};
+
+export default PagosDashboard;
